57f03ec3e90e0c702d6c214505a48790df325ca4
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2021-01-27 10:38:53 +0800

    got the main key press done

--- learning/curses_learn.py
+++ learning/curses_learn.py
@@ -1,36 +1,22 @@
 import curses
 import time
 
-stdscr = curses.initscr()
+def setup(stdscr: curses.window):
+    t = 'Welcome to the test run baby'
 
-# Setup scr
-curses.noecho()
-curses.curs_set(0)
-curses.cbreak()
+def main(stdscr: curses.window):
+    while True:
+        u = stdscr.getch()
+        if u == curses.KEY_UP:
+            stdscr.addstr(0, 0, 'Pressed key up')
+            stdscr.chgat(-1)
+        elif u == curses.KEY_DOWN:
+            stdscr.addstr(0, 0, 'Pressed key down')
+            stdscr.chgat(-1)
+        else:
+            stdscr.addstr(0, 0, 'Nani what key')
+            stdscr.chgat(-1)
+        stdscr.noutrefresh()
+        curses.doupdate()
 
-if curses.has_colors():
-    curses.start_color()
-
-# Main things happen
-# text = 'a' * (curses.COLS - 1) + 'b'
-# text1 = 'a' * (curses.COLS) + 'b'
-# stdscr.addstr(10, 0, text)
-# stdscr.addstr(11, 0, text1, curses.A_REVERSE)
-
-# Colors
-curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
-
-t = 'Hello world'
-# stdscr.addstr(5, 5, t, curses.A_REVERSE | curses.color_pair(1))
-stdscr.addstr(5, 5, t, curses.color_pair(1))
-
-
-stdscr.refresh()
-time.sleep(3)
-
-# Tear down
-curses.echo()
-curses.curs_set(1)
-curses.nocbreak()
-
-curses.endwin()
+curses.wrapper(main)
