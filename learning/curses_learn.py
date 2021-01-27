35f6d1e9d2556b7ff70237e7a6fd479a3b82b22e
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2021-01-27 10:21:00 +0800

    curses again

--- learning/curses_learn.py
+++ learning/curses_learn.py
@@ -1,11 +1,36 @@
 import curses
 import time
 
-def main(stdscr):
-    curses.curs_set(0)
-    stdscr.addstr(5, 5, "Hellow world")
-    stdscr.refresh()
-    time.sleep(3)
-    
+stdscr = curses.initscr()
 
-curses.wrapper(main)
+# Setup scr
+curses.noecho()
+curses.curs_set(0)
+curses.cbreak()
+
+if curses.has_colors():
+    curses.start_color()
+
+# Main things happen
+# text = 'a' * (curses.COLS - 1) + 'b'
+# text1 = 'a' * (curses.COLS) + 'b'
+# stdscr.addstr(10, 0, text)
+# stdscr.addstr(11, 0, text1, curses.A_REVERSE)
+
+# Colors
+curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+
+t = 'Hello world'
+# stdscr.addstr(5, 5, t, curses.A_REVERSE | curses.color_pair(1))
+stdscr.addstr(5, 5, t, curses.color_pair(1))
+
+
+stdscr.refresh()
+time.sleep(3)
+
+# Tear down
+curses.echo()
+curses.curs_set(1)
+curses.nocbreak()
+
+curses.endwin()
