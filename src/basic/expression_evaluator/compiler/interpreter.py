52ccb077cdd823f477252aa51e2cc49df169fd42
SherisseTJW <tanjingwensherisse@gmail.com>
2021-02-03 17:43:10 +0800

    adding interpreter code from proof-of-concept

--- src/basic/expression_evaluator/compiler/interpreter.py
+++ src/basic/expression_evaluator/compiler/interpreter.py
@@ -1,8 +1,34 @@
 
 from ..tokens import *
+from ..nodes import *
 from .lexer import Lexer
 from .parser import Parser
 
-class Interpreter:
-    def __init__(self):
-        pass
+class Interpreter(NodeVisitor):
+    def error(self):
+        raise Exception("Error intepreting expression")
+
+    def visit_Number_Node(self, node):
+        return node.token_value
+
+    def visit_BinaryOp_Node(self, node):
+        token_type = node.op.token_type
+
+        if token_type == PLUS:
+            return self.visit(node.left_term) + self.visit(node.right_term)
+        if token_type == MINUS:
+            return self.visit(node.left_term) - self.visit(node.right_term)
+        if token_type == MUL:
+            return self.visit(node.left_term) * self.visit(node.right_term)
+        if token_type == DIV:
+            return self.visit(node.left_term) / self.visit(node.right_term)
+        if token_type == POWER:
+            return self.visit(node.left_term) ** self.visit(node.right_term)
+
+    def interpret(self):
+        ast = self.parser.parse()
+
+        if ast == None:
+            return None
+
+        return self.visit(ast)
