d866f43ad4da9cd73cc535f04eeeee7df549e3e4
chuanhao01 <34702921+chuanhao01@users.noreply.github.com>
2021-02-14 02:38:26 +0800

    added NodeVisitor, fixed import
    added ndoe visitor and ast into the module init

--- src/advance/expression_evaluator/nodes/__init__.py
+++ src/advance/expression_evaluator/nodes/__init__.py
@@ -1,5 +1,10 @@
+# AST nodes
+from .ast import AST
 from .binary_op_node import BinaryOp_Node
 from .function_node import Function_Node
 from .number_node import Number_Node
 from .string_node import String_Node
 from .unary_op_node import Unary_Node
+
+# Node Visitor
+from .node_visitor import NodeVisitor
